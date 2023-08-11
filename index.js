35e6a4c7217b728270db7def6fda08dd78057d79
Abhi <abhiramims333@gmail.com>
2023-08-11 11:13:04 -0400

    Change code for port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 3030;
+const port = process.env.PORT || 5000;
 const _ = require("lodash");
 // const date = new Date();
 var newI = [];
@@ -142,5 +142,5 @@ app.post("/delete",function(req,res){
 
 
 app.listen(port, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${port}`);
 });
